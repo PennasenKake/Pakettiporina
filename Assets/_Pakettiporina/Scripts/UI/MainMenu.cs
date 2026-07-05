215af892915909f84ebd4aaa9b96c512649b3db7
PennasenKake <121464958+PennasenKake@users.noreply.github.com>
2026-07-06 00:54:20 +0300

    Aloitusnakyma: taustakuva, MainMenu-scene ja Pelaa-nappi

---
+++ Assets/_Pakettiporina/Scripts/UI/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Pakettiporina
+{
+    // Aloitusnakyman logiikka: "Pelaa" lataa pelin scenen, "Lopeta" sulkee sovelluksen.
+    // Liita tyhjaan objektiin tai Canvasiin aloitusnakymassa.
+    public class MainMenu : MonoBehaviour
+    {
+        [Tooltip("Pelin scenen nimi. Lisaa scene Build Settingsiin! (nyt esim. SampleScene)")]
+        public string gameSceneName = "SampleScene";
+
+        void Start()
+        {
+            Debug.Log("[MainMenu] Aloitusnakyma valmis.");
+        }
+
+        // Kytke tama "Pelaa"-napin OnClickiin.
+        public void PlayGame()
+        {
+            Debug.Log("[MainMenu] Ladataan peli: " + gameSceneName);
+            SceneManager.LoadScene(gameSceneName);
+        }
+
+        // Kytke tama halutessasi "Lopeta"-napin OnClickiin.
+        public void QuitGame()
+        {
+            Debug.Log("[MainMenu] Suljetaan peli.");
+            Application.Quit();
+        }
+    }
+}
